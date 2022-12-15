e7e738983b1a8f313a503762ce20f8a7fbc91ecd
immangalang <imman.galang@gmail.com>
2022-12-15 18:10:36 +0800

    creating a custom observable

--- obs-03-complete-custom-observable/src/app/home/home.component.ts
+++ obs-03-complete-custom-observable/src/app/home/home.component.ts
@@ -17,27 +17,27 @@ export class HomeComponent implements OnInit, OnDestroy {
     // this.firstObsSubscription = interval(1000).subscribe(count => {
     //   console.log(count);
     // });
-    const customIntervalObservable = Observable.create(observer => {
+    const customIntervalObservable = new Observable(observer => {
       let count = 0;
       setInterval(() => {
         observer.next(count);
-        if (count === 5) {
-          observer.complete();
-        }
-        if (count > 3) {
-          observer.error(new Error('Count is greater 3!'));
-        }
+        // if (count === 5) {
+        //   observer.complete();
+        // }
+        // if (count > 3) {
+        //   observer.error(new Error('Count is greater 3!'));
+        // }
         count++;
       }, 1000);
     });
 
     this.firstObsSubscription = customIntervalObservable.subscribe(data => {
       console.log(data);
-    }, error => {
-      console.log(error);
-      alert(error.message);
-    }, () => {
-      console.log('Completed!');
+      // }, error => {
+      //   console.log(error);
+      //   alert(error.message);
+      // }, () => {
+      //   console.log('Completed!');
     });
   }
 
